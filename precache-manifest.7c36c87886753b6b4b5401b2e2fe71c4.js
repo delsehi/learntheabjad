738c09d5affe704849c9b3d292796b8edacb9112
Delfi Sehidic <ds222qe@student.lnu.se>
2020-05-25 15:35:15 +0200

    Updates

---
+++ precache-manifest.7c36c87886753b6b4b5401b2e2fe71c4.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e72b716c3bc33d19dc82e1907da5eb",
+    "url": "/delsehi/learntheabjad/index.html"
+  },
+  {
+    "revision": "fa2697bcee4bd679bc49",
+    "url": "/delsehi/learntheabjad/static/css/main.171a8da7.chunk.css"
+  },
+  {
+    "revision": "21ea9c3c8de3751a992a",
+    "url": "/delsehi/learntheabjad/static/js/2.03706246.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/delsehi/learntheabjad/static/js/2.03706246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa2697bcee4bd679bc49",
+    "url": "/delsehi/learntheabjad/static/js/main.30ee72ab.chunk.js"
+  },
+  {
+    "revision": "071dc1406c0d14755464",
+    "url": "/delsehi/learntheabjad/static/js/runtime-main.b499db7b.js"
+  },
+  {
+    "revision": "d6258c909ddf41a934d93b1cf92da919",
+    "url": "/delsehi/learntheabjad/static/media/arab01.d6258c90.wav"
+  },
+  {
+    "revision": "afca4631751ee00537f7f901a2d4b242",
+    "url": "/delsehi/learntheabjad/static/media/arab02.afca4631.wav"
+  },
+  {
+    "revision": "0ddf16a398e95ddaa144e90d14673e4e",
+    "url": "/delsehi/learntheabjad/static/media/arab03.0ddf16a3.wav"
+  },
+  {
+    "revision": "87fc350be89355d8f7883285a31eec84",
+    "url": "/delsehi/learntheabjad/static/media/arab04.87fc350b.wav"
+  },
+  {
+    "revision": "43f0820ef9070d9d586ac824f0078aeb",
+    "url": "/delsehi/learntheabjad/static/media/arab05.43f0820e.wav"
+  },
+  {
+    "revision": "7b0002c86d03d48291c5a9d4df5fc482",
+    "url": "/delsehi/learntheabjad/static/media/arab06.7b0002c8.wav"
+  },
+  {
+    "revision": "424d976c399773e3aba2066a77a14327",
+    "url": "/delsehi/learntheabjad/static/media/arab07.424d976c.wav"
+  },
+  {
+    "revision": "770e2dfa07f687f23a616a188656f19b",
+    "url": "/delsehi/learntheabjad/static/media/arab08.770e2dfa.wav"
+  },
+  {
+    "revision": "0100bb798f6f13083e4104736036362f",
+    "url": "/delsehi/learntheabjad/static/media/arab09.0100bb79.wav"
+  },
+  {
+    "revision": "661f0b67b5a4eec2dc648622f079b7eb",
+    "url": "/delsehi/learntheabjad/static/media/arab10.661f0b67.wav"
+  },
+  {
+    "revision": "4922617c8d747a871c24b4535d28b606",
+    "url": "/delsehi/learntheabjad/static/media/arab11.4922617c.wav"
+  },
+  {
+    "revision": "98f8ea934e0aa80b3274be1f7fc653bf",
+    "url": "/delsehi/learntheabjad/static/media/arab12.98f8ea93.wav"
+  },
+  {
+    "revision": "0c8ab37ce0a9057edd062c38339e622b",
+    "url": "/delsehi/learntheabjad/static/media/arab13.0c8ab37c.wav"
+  },
+  {
+    "revision": "7160dabc697021fbf615e6292e0a8ef2",
+    "url": "/delsehi/learntheabjad/static/media/arab14.7160dabc.wav"
+  },
+  {
+    "revision": "2f77b419a4564a9b920d95bbc72178dc",
+    "url": "/delsehi/learntheabjad/static/media/arab15.2f77b419.wav"
+  },
+  {
+    "revision": "77414ac8f2436f1e0615880f5d3ea284",
+    "url": "/delsehi/learntheabjad/static/media/arab16.77414ac8.wav"
+  },
+  {
+    "revision": "ed473ed012a3377e6f07a043f61a0a2a",
+    "url": "/delsehi/learntheabjad/static/media/arab17.ed473ed0.wav"
+  },
+  {
+    "revision": "9b3165ba2775c8461d3f2752c9451945",
+    "url": "/delsehi/learntheabjad/static/media/arab18.9b3165ba.wav"
+  },
+  {
+    "revision": "c1d29e57593b755e946eefa633c2ed80",
+    "url": "/delsehi/learntheabjad/static/media/arab19.c1d29e57.wav"
+  },
+  {
+    "revision": "ce83f2b497672fabdeda5c5562e0f323",
+    "url": "/delsehi/learntheabjad/static/media/arab20.ce83f2b4.wav"
+  },
+  {
+    "revision": "32a123de221df0eeee31e0b31c81d287",
+    "url": "/delsehi/learntheabjad/static/media/arab21.32a123de.wav"
+  },
+  {
+    "revision": "9644adbda06de7ee4bbcc81f0b05b499",
+    "url": "/delsehi/learntheabjad/static/media/arab22.9644adbd.wav"
+  },
+  {
+    "revision": "fe167d908598312c75fdf74d84264d89",
+    "url": "/delsehi/learntheabjad/static/media/arab23.fe167d90.wav"
+  },
+  {
+    "revision": "d978eca4196721b8da0b77c64bf63903",
+    "url": "/delsehi/learntheabjad/static/media/arab24.d978eca4.wav"
+  },
+  {
+    "revision": "17c0386b48919c76dbae85e4f0911c33",
+    "url": "/delsehi/learntheabjad/static/media/arab25.17c0386b.wav"
+  },
+  {
+    "revision": "b91e460b67a8959b2942d1f1169fd588",
+    "url": "/delsehi/learntheabjad/static/media/arab26.b91e460b.wav"
+  },
+  {
+    "revision": "611385907d15bff6bc55390df6798493",
+    "url": "/delsehi/learntheabjad/static/media/arab27.61138590.wav"
+  },
+  {
+    "revision": "fc2f14bfecd7655bca820f27d4bdb0b9",
+    "url": "/delsehi/learntheabjad/static/media/arab28.fc2f14bf.wav"
+  }
+]);
