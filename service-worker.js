f7372c50faa1135b6f11d5c1188bd4abcfbe4d9b
Delfi Sehidic <ds222qe@student.lnu.se>
2020-05-25 15:36:39 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/delsehi/learntheabjad/precache-manifest.7c36c87886753b6b4b5401b2e2fe71c4.js"
+  "/learntheabjad/precache-manifest.9b90b5e7f05cfe6c5b00b6a5aebc4923.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/delsehi/learntheabjad/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learntheabjad/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
